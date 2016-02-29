4d7a2d3d6b67ea3a8d2ce545e50baff9d7c5908c
dmitry.senin <dmitry.senin@akvelon.com>
2016-02-29 12:20:33 +0300

    Reporting System. Implement method to collect notification from protocol by its type.

--- ReportingSystem.Reports/ReportProtocol.cs
+++ ReportingSystem.Reports/ReportProtocol.cs
@@ -1,6 +1,7 @@
 ﻿namespace ReportingSystem.Reports
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     /// <summary>
     /// Result protocol of reporting.
@@ -22,5 +23,19 @@
         /// Notifications detected by system during reporting.
         /// </summary>
         public List<Notification> Notifications { get; set; }
+
+        /// <summary>
+        /// Collect notification by type.
+        /// </summary>
+        /// <param name="type">Target type of notification.</param>
+        /// <returns>Collection of notifications contains records only with specified type or empty collection.</returns>
+        public List<Notification> NotificationsByType(NotificationType type)
+        {
+            var notifications = (from notification in this.Notifications
+                                 where notification.Type == type
+                                 select notification).ToList();
+
+            return notifications;
+        }
     }
 }

e1841193ee943c543f9ff47d6b01cadabedca897
dmitry.senin <dmitry.senin@akvelon.com>
2016-02-29 09:54:40 +0300

    Reporting System. Create class to represent notification of reporting system.

---
+++ ReportingSystem.Reports/Notification.cs
@@ -0,0 +1,29 @@
+﻿namespace ReportingSystem.Reports
+{
+    /// <summary>
+    /// Represents notification of reporting system about whatever it does.
+    /// </summary>
+    public class Notification
+    {
+        /// <summary>
+        /// Describes details of notification.
+        /// </summary>
+        public readonly string Description;
+
+        /// <summary>
+        /// Describe dangerous of event represented by this notification.
+        /// </summary>
+        public readonly NotificationType Type;
+
+        /// <summary>
+        /// Create instance of class <see cref="Notification"/> class.
+        /// </summary>
+        /// <param name="description">Message describing notification.</param>
+        /// <param name="type">Type of notification.</param>
+        public Notification(string description, NotificationType type)
+        {
+            this.Description = description;
+            this.Type = type;
+        }
+    }
+}

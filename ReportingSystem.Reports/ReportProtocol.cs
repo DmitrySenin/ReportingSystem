992cc2cfc123c6c76b50621b76512312cfbbfd24
dmitry.senin <dmitry.senin@akvelon.com>
2016-02-29 10:02:47 +0300

    Reporting System. Design class of reporting result.

---
+++ ReportingSystem.Reports/ReportProtocol.cs
@@ -0,0 +1,26 @@
+﻿namespace ReportingSystem.Reports
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Result protocol of reporting.
+    /// </summary>
+    /// <typeparam name="T">Type of returned result.</typeparam>
+    public class ReportProtocol<T>
+    {
+        /// <summary>
+        /// Gets/sets target result of report.
+        /// </summary>
+        public T Result { get; set; }
+
+        /// <summary>
+        /// Gets/sets flag that identifies success of reporting.
+        /// </summary>
+        public bool IsSucceed { get; set; }
+
+        /// <summary>
+        /// Notifications detected by system during reporting.
+        /// </summary>
+        public List<Notification> Notifications { get; set; }
+    }
+}

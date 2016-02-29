fafc7ae4c5af7beb8bfcee86842b3b007545f02f
dmitry.senin <dmitry.senin@akvelon.com>
2016-02-29 09:42:46 +0300

    Reporting System. Design system's notification classifier.

---
+++ ReportingSystem.Reports/NotificationType.cs
@@ -0,0 +1,24 @@
+﻿namespace ReportingSystem.Reports
+{
+    /// <summary>
+    /// Helps classify notifications.
+    /// </summary>
+    public enum NotificationType
+    {
+        /// <summary>
+        /// Notification is unresolved error.
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// Notification is resolved error.
+        /// </summary>
+        Warning,
+
+        /// <summary>
+        /// Notification represents information about some action of system
+        /// or not dangerous accident.
+        /// </summary>
+        Message
+    }
+}

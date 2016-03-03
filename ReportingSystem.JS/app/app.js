582211b36cd0c0fdb29e5856d30acf9f736df5d5
dmitry.senin <dmitry.senin@akvelon.com>
2016-03-03 16:17:56 +0300

    Core of reporting system. Collect all modules of core functionality to application obejct.

--- ReportingSystem.JS/app/app.js
+++ ReportingSystem.JS/app/app.js
@@ -1,3 +1,13 @@
-var app = {
-	ReportingSystem : {}
+var app = {};
+
+app.ReportingSystem = {
+	StampType: require('./reports/StampType.js'),
+	EmployerTimeStamp: require('./reports/EmployerTimeStamp.js'),
+	NotificationType: require('./reports/NotificationType.js'),
+	Notification: require('./reports/Notification.js'),
+	ReportProtocol: require('./reports/ReportProtocol.js')
+};
+
+module.exports = {
+	app: app
 };

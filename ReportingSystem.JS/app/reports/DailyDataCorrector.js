0268b41801bda920bb731046204eaf8848a7ef2c
dmitry.senin <dmitry.senin@akvelon.com>
2016-03-03 19:09:09 +0300

    Gather stamps. Extract messages to special object.

--- ReportingSystem.JS/app/reports/DailyDataCorrector.js
+++ ReportingSystem.JS/app/reports/DailyDataCorrector.js
@@ -5,18 +5,20 @@ var NotificationType = require('./NotificationType.js');
 var Notification = require('./Notification.js');
 var ReportProtocol = require('./ReportProtocol.js');
 
-var UndefinedSourceDataReference = "Source of data can't be null or undefined.";
-var NotificationsIsNotArray = "Notifications should be an array.";
-var StampsIsNotArray = "System can work only with array of time stamps.";
-var SourceDataIncorrectDataFormat = "Source of stamps should return array of employer stamps.";
-var UndefinedStampsCollection = "Collection of stamps can't be null or undefined.";
-var FirstInStampNotFound = "First In-stamp was not found.";
-var BeginDayAsInStampAdded = "Begin of target day was added as first In-stamp.";
-var LastOutStampNotFound = "Last out-stamp was not found.";
-var FirstOutNextDayAsLast = "First Out-stamp of next day was added as last Out-stamp.";
-var EndDayAsLastOutStamp = "End of target day was added as last Out-stamp.";
-var OneOfEqualStampsRemoved = "Equal stamps was found. One of them was removed.";
-var NewStampInMiddleBetweenSameType = "Found two followed stamps of one type. Add new between them at the middle.";
+var messages = {
+	UndefinedSourceDataReference : "Source of data can't be null or undefined.";
+	NotificationsIsNotArray : "Notifications should be an array.";
+	StampsIsNotArray : "System can work only with array of time stamps.";
+	SourceDataIncorrectDataFormat : "Source of stamps should return array of employer stamps.";
+	UndefinedStampsCollection : "Collection of stamps can't be null or undefined.";
+	FirstInStampNotFound : "First In-stamp was not found.";
+	BeginDayAsInStampAdded : "Begin of target day was added as first In-stamp.";
+	LastOutStampNotFound : "Last out-stamp was not found.";
+	FirstOutNextDayAsLast : "First Out-stamp of next day was added as last Out-stamp.";
+	EndDayAsLastOutStamp : "End of target day was added as last Out-stamp.";
+	OneOfEqualStampsRemoved : "Equal stamps was found. One of them was removed.";
+	NewStampInMiddleBetweenSameType : "Found two followed stamps of one type. Add new between them at the middle."
+};
 
 /**
  * Create object which collect and correct data for daily reports.
@@ -25,7 +27,7 @@ var NewStampInMiddleBetweenSameType = "Found two followed stamps of one type. Ad
 function DailyDataCorrector(dataSource) {
 	
 	if(dataSource === undefined || dataSource === null) {
-		throw new Error(UndefinedSourceDataReference);
+		throw new Error(messages.UndefinedSourceDataReference);
 	}
 
 	/**
@@ -39,14 +41,14 @@ function DailyDataCorrector(dataSource) {
 	this.CollectStampsForDailyReport = function(employerID, day, notifications) {
 
 		if(!Array.isArray(notifications)) {
-			throw new Error(NotificationsIsNotArray);
+			throw new Error(messages.NotificationsIsNotArray);
 		}
 
 		// Get stamps from source.
 		stamps = dataSource.GetByEmployerIDForDay(employerID, day);
 
 		if(Array.isArray(stamps)) {
-			throw new Error(SourceDataIncorrectDataFormat);
+			throw new Error(messages.SourceDataIncorrectDataFormat);
 		}
 
 		// No stamps for day.
@@ -71,22 +73,22 @@ function DailyDataCorrector(dataSource) {
 	 */
 	this._verifyFirstStamp = function(stamps, employerID, day, notifications) {
 		if(!Array.isArray(notifications)) {
-			throw new Error(NotificationsIsNotArray);
+			throw new Error(messages.NotificationsIsNotArray);
 		}
 
 		if(stamps === undefined || stamps === null) {
-			throw new Error(UndefinedStampsCollection);
+			throw new Error(messages.UndefinedStampsCollection);
 		}
 
 		if(!Array.isArray(stamps)) {
-			throw new Error(StampsIsNotArray);
+			throw new Error(messages.StampsIsNotArray);
 		}
 
 		// if there is not first In-stamp
 		// add it as begging of target day.
 		if(stamps.length === 0 || stamps[0].Type !== StampType.In) {
-			notifications.push(new Notification(FirstInStampNotFound, NotificationType.Warning));
-			notifications.push(new Notification(BeginDayAsInStampAdded, NotificationType.Message));
+			notifications.push(new Notification(messages.FirstInStampNotFound, NotificationType.Warning));
+			notifications.push(new Notification(messages.BeginDayAsInStampAdded, NotificationType.Message));
 
 			beggingOfTargetDay = new Date(day.getYear(), day.getMonth(), day.getDate(), 0, 0, 0, 0);
 
@@ -106,22 +108,22 @@ function DailyDataCorrector(dataSource) {
 	 */
 	this._verifyLastStamp = function(stamps, employerID, day, notifications) {
 		if(!Array.isArray(notifications)) {
-			throw new Error(NotificationsIsNotArray);
+			throw new Error(messages.NotificationsIsNotArray);
 		}
 
 		if(stamps === undefined || stamps === null) {
-			throw new Error(UndefinedStampsCollection);
+			throw new Error(messages.UndefinedStampsCollection);
 		}
 
 		if(!Array.isArray(stamps)) {
-			throw new Error(StampsIsNotArray);
+			throw new Error(messages.StampsIsNotArray);
 		}
 
 		// if last stamps is not Out-stamp
 		// then find stamps for next day and check first one.
 		// or add end of target day as last Out-stamp.
 		if(stamps.length == 0 || stamps[stamps.length - 1].Type !== StampType.Out) {
-			notifications.push(new Notification(LastOutStampNotFound, NotificationType.Warning));
+			notifications.push(new Notification(messages.LastOutStampNotFound, NotificationType.Warning));
 
 			nextDay = new Date(day);
 			nextDay.setDate(nextDay.getDate() + 1);
@@ -137,11 +139,14 @@ function DailyDataCorrector(dataSource) {
 					&& stampsForNextDay.length > 0
 					&& stampsForNextDay[0].Type == StampType.Out 
 					&& stampsForNextDay[0].Time <= nextDayFindingDate) {
-				notifications.push(new Notification(FirstOutNextDayAsLast, NotificationType.Message));
+
+				notifications.push(new Notification(messages.FirstOutNextDayAsLast, NotificationType.Message));
 
 				stamps.push(stampsForNextDay[0]);
+
 			} else {
-				notifications.push(new Notification(EndDayAsLastOutStamp, NotificationType.Message));
+
+				notifications.push(new Notification(messages.EndDayAsLastOutStamp, NotificationType.Message));
 
 				nextDay.setHours(23, 59, 59, 999);
 
@@ -161,16 +166,17 @@ function DailyDataCorrector(dataSource) {
 	 * @param  {Array} notifications Notifications that will be completed during processing.
 	 */
 	this._verifyStampsSequence = function(stamps, employerID, day, notifications) {
+		
 		if(!Array.isArray(notifications)) {
-			throw new Error(NotificationsIsNotArray);
+			throw new Error(messages.NotificationsIsNotArray);
 		}
 
 		if(stamps === undefined || stamps === null) {
-			throw new Error(UndefinedStampsCollection);
+			throw new Error(messages.UndefinedStampsCollection);
 		}
 
 		if(!Array.isArray(stamps)) {
-			throw new Error(StampsIsNotArray);
+			throw new Error(messages.StampsIsNotArray);
 		}
 
 		i = 0;
@@ -183,7 +189,7 @@ function DailyDataCorrector(dataSource) {
 
 				// Stamps' times are equal.
 				if(stamps[i].Time === stamps[i + 1].Time) {
-					notifications.push(new Notification(OneOfEqualStampsRemoved, NotificationType.Warning));
+					notifications.push(new Notification(messages.OneOfEqualStampsRemoved, NotificationType.Warning));
 
 					stamps.splice(i + 1, 1);
 
@@ -191,7 +197,7 @@ function DailyDataCorrector(dataSource) {
                     // new (i + 1) element can be same type and time again.
                     // So we need to compare i-th and (i + 1)-th again.
 				} else {
-					notifications.push(new Notification(NewStampInMiddleBetweenSameType, NotificationType.Warning));
+					notifications.push(new Notification(messages.NewStampInMiddleBetweenSameType, NotificationType.Warning));
 
 					diffInMilliseconds = stamps[i + 1].Time - stamps[i].Time;
 

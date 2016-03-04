0c3c437c8c09fda2f609d12559aac5d539eaba88
dmitry.senin <dmitry.senin@akvelon.com>
2016-03-04 10:12:05 +0300

    [JS]Respites report. Modify application file to check computation of respites.

--- ReportingSystem.JS/app/app.js
+++ ReportingSystem.JS/app/app.js
@@ -6,8 +6,11 @@ app.ReportingSystem = {
 	NotificationType: require('./reports/NotificationType.js'),
 	Notification: require('./reports/Notification.js'),
 	ReportProtocol: require('./reports/ReportProtocol.js'),
-	TimeOfWorkForDayReport: require('./reports/TimeOfWorkForDayReport.js')
+	TimeOfWorkForDayReport: require('./reports/TimeOfWorkForDayReport.js'),
+	RespitesForDayReport: require('./reports/RespitesForDayReport.js')
 };
+
+var timespan = require('timespan');
 
 var targetDay = new Date(2016, 3, 3);
 var source = {
@@ -25,7 +28,14 @@ var workTimeReporter = new app.ReportingSystem.TimeOfWorkForDayReport(source);
 protocol = workTimeReporter.CreateReport(1, targetDay);
 
 console.log(protocol);
-console.log(protocol.Result.totalHours());
+console.log('Total hours = ' + protocol.Result.totalHours());
+
+var respitesReporter = new app.ReportingSystem.RespitesForDayReport.RespitesForDay(source);
+var maxDuration = timespan.fromMinutes(16);
+
+protocol = respitesReporter.CreateReport(1, targetDay, maxDuration);
+
+console.log(protocol);
 
 module.exports = {
 	app: app

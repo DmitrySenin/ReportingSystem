067a6296e50ed22bf3a1f3915954127446989934
dmitry.senin <dmitry.senin@akvelon.com>
2016-03-04 15:23:50 +0300

    [JS]Time of work tests. Test system of computaion time of work with incorrect data.

--- ReportingSystem.JS/app/test/TimeOfWorkReport.CreateReport.Tests.js
+++ ReportingSystem.JS/app/test/TimeOfWorkReport.CreateReport.Tests.js
@@ -68,4 +68,54 @@ describe("TimeOfWorkReport.CreateReport", function() {
 		protocol.Result.should.deep.equal(expectedTimeOfWork);
 	});
 
+	it("Should correct compute time of work since there is no stamps of employer for day.", function() {
+
+		// Arrange
+	
+		var targetEmployerID = 1;
+
+		// 03.04.2016
+		var targetDay = new Date(2016, 3, 3);
+
+		stamps = [
+			// Out-stamp at 9.00 am
+			new EmployerTimeStamp( targetEmployerID, StampType.Out, new Date(targetDay.setHours(9)) ),
+
+			// Out-stamp at 10.00 am
+			new EmployerTimeStamp( targetEmployerID, StampType.Out, new Date(targetDay.setHours(10)) ),
+
+			// In-stamp at 6.00 pm
+			new EmployerTimeStamp( targetEmployerID, StampType.In, new Date(targetDay.setHours(18)) ),
+
+			// In-stamp at 6.00 pm
+			new EmployerTimeStamp( targetEmployerID, StampType.In, new Date(targetDay.setHours(18)) ),
+		];
+
+		// Create fake source.
+		var source = new FakeStampsSource.FakeStampsSource(stamps);
+		var reporter = new TimeOfWorkReport(source);
+
+		// Result should be like this because 
+		// system makes the following changes in data:
+		// 		1. Creates In-stamp at 12.00 am of target day
+		// 		2. Insert In-stamp at 9.30 am between Out-stamp at 9.00 am 
+		//												and Out-stamp at 10.00 am
+		//		3. Delete one of In-stamp at 6.00 pm
+		// 		4. Creates Out-stamps at 11.59.59:999 pm of target day
+		// 	So employer come  in at 12.00 am, 
+		// 	than ho out at 9.00 am (9 hours of work),
+		// 	come in again at 9.30 am,
+		// 	go out at 10.00 am (plus 30 minutes = 9 hours and 30 minutes of work),
+		// 	come in at 6.pm and go out at 11.59.59:999 pm(plus 5.59.59:999 hours of work).
+		// 	9.30 + 5.59.59:999 = 15.29.59:999 hours of work.
+		var expectedTimeOfWork = new TimeSpan.TimeSpan(999, 59, 29, 15);
+
+		// Act
+		var protocol = reporter.CreateReport(targetEmployerID, targetDay);
+
+		// Assert
+		protocol.IsSucceed.should.equal(true);
+		protocol.Result.should.deep.equal(expectedTimeOfWork);
+	});
+
 });

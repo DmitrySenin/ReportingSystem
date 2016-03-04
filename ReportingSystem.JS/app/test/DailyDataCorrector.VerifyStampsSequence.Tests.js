837505dd1fb4358b88d93d0a654c4c36273bbefe
dmitry.senin <dmitry.senin@akvelon.com>
2016-03-04 14:20:08 +0300

    [JS]Gather stamps tests. Test that new stamp will be added in the middle of two with same type.

---
+++ ReportingSystem.JS/app/test/DailyDataCorrector.VerifyStampsSequence.Tests.js
@@ -0,0 +1,48 @@
+var should = require('chai').should();
+
+var StampType = require('../reports/StampType.js');
+var EmployerTimeStamp = require('../reports/EmployerTimeStamp.js');
+var DailyDataCorrector = require('../reports/DailyDataCorrector.js');
+
+var FakeStampsSource = require('./FakeStampsSource.js');
+
+describe("DailyDataCorrector.VerifyStampsSequence", function() {
+
+	it("Should add stamps at the middle of same typed ones.", function() {
+
+		var targetEmployerID = 1;
+
+		// 03.04.2016
+		var targetDay = new Date(2016, 3, 3);
+
+		stamps = [
+			// Out-stamp at 9.00 am
+			new EmployerTimeStamp( targetEmployerID, StampType.Out, new Date(targetDay.setHours(9)) ),
+
+			// Out-stamp at 10.00 am
+			new EmployerTimeStamp( targetEmployerID, StampType.Out, new Date(targetDay.setHours(10)) ),
+		];
+
+		// Create fake source.
+		var source = new FakeStampsSource.FakeStampsSource(stamps);
+
+		var dailyDataCorrector = new DailyDataCorrector(source);
+		var notifications = [];
+
+		var expectedSize = stamps.length + 1;
+		var expectedStamps = [
+			stamps[0],
+
+			// In-stamp at 9.30 am
+			new EmployerTimeStamp( targetEmployerID, StampType.Out, new Date(targetDay.setHours(9, 30)) ),
+
+			stamps[1]
+		];
+
+		dailyDataCorrector._verifyStampsSequence(stamps, targetEmployerID, targetDay, notifications);
+
+		stamps.length.should.equal(expectedSize);
+		stamps.should.deep.equal(expectedStamps);
+	});
+	
+});

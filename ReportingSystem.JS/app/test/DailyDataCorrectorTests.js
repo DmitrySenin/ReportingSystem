847f206d65bc3c8546345ebc26079ed1ffd53e62
dmitry.senin <dmitry.senin@akvelon.com>
2016-03-04 12:20:54 +0300

    [JS]Gather stamps tests. Test that begging of thaget day added if no first In-stamp

--- ReportingSystem.JS/app/test/DailyDataCorrectorTests.js
+++ ReportingSystem.JS/app/test/DailyDataCorrectorTests.js
@@ -1,12 +1,14 @@
 var should = require('chai').should();
 
+var StampType = require('../reports/StampType.js');
+var EmployerTimeStamp = require('../reports/EmployerTimeStamp.js');
 var DailyDataCorrector = require('../reports/DailyDataCorrector.js');
 
 describe("DailyDataCorrector", function() {
 
 	// Tests of constructor.
-	describe("DailyDataCorrector", function() {
-		
+	describe("#DailyDataCorrector", function() {
+
 		it("Should throw error if source of data is undefined.", function() {
 			var dataSource = undefined;
 			(() => new DailyDataCorrector(dataSource)).should.throw(Error);
@@ -15,6 +17,40 @@ describe("DailyDataCorrector", function() {
 		it("Should throw error if source of data is null.", function() {
 			var dataSource = null;
 			(() => new DailyDataCorrector(dataSource)).should.throw(Error);
+		});
+	});
+
+	describe("#VerifyFirstStamp", function() {
+
+		it("Should add begging of target day as first In-stamp if there is no one.", function() {
+
+			var targetEmployerID = 1;
+
+			// 03.03.2016
+			var targetDay = new Date(2016, 3, 3);
+
+			stamps = [
+				// Out-stamp at 9.00 am
+				new EmployerTimeStamp( targetEmployerID, StampType.Out, new Date(targetDay.setHours(9)) )
+			];
+
+			// Create fake source.
+			var source = { 
+				GetByEmployerIDForDay() {
+					return stamps;
+				}
+			};
+
+			var dailyDataCorrector = new DailyDataCorrector(source);
+			var notifications = [];
+
+			var expectedSize = stamps.length + 1;
+			var expectedStamp = new EmployerTimeStamp(targetEmployerID, StampType.In, new Date(targetDay.setHours(0)));
+
+			dailyDataCorrector._verifyFirstStamp(stamps, targetEmployerID, targetDay, notifications);
+
+			stamps.length.should.equal(expectedSize);
+			stamps[0].should.deep.equal(expectedStamp);
 		});
 	});
 });

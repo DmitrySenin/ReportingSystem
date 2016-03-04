80a80a7ce14b0ff77475db6ddd5acba9d40071cd
dmitry.senin <white.lord.mane@gmail.com>
2016-03-04 21:02:23 +0400

    [JS]Respites tests. Test system to not find any respites if maximum duration of respite equals zero.

--- ReportingSystem.JS/app/test/RespitesForDayReport.CreateReport.Tests.js
+++ ReportingSystem.JS/app/test/RespitesForDayReport.CreateReport.Tests.js
@@ -75,4 +75,48 @@ describe("RespitesForDayReport.CreateReport", function() {
 
 	});
 
+	it("Should not find any respites since maximum respite duration equals 0.", function() {
+
+		var targetEmployerID = 1;
+
+		// 03.04.2016
+		var targetDay = new Date(2016, 3, 3);
+
+		var maxRespiteDuration = new TimeSpan.TimeSpan(0); 
+
+		stamps = [
+			// In-stamp at 9.00 am
+			new EmployerTimeStamp( targetEmployerID, StampType.In, new Date(targetDay.setHours(9)) ),
+
+			// Out-stamp at 10.00 am
+			new EmployerTimeStamp( targetEmployerID, StampType.Out, new Date(targetDay.setHours(10)) ),
+
+			// In-stamp at 10.01 am
+			new EmployerTimeStamp( targetEmployerID, StampType.In, new Date(targetDay.setHours(10)) ),
+
+			// Out-stamp at 6.00 pm
+			new EmployerTimeStamp( targetEmployerID, StampType.Out, new Date(targetDay.setHours(18)) ),
+		];
+
+		// Create fake source.
+		var source = new FakeStampsSource.FakeStampsSource(stamps);
+		var reporter = new RespitesForDayReport.RespitesForDay(source);
+
+		// No expected respites 
+		// Source of data should sort stamp by its type too
+		// so In-stamp at 10.00 am will at the 1st position(start with 0)
+		// and Out-stamp at 10.00 am will at the 2nd position
+		// so system will not compute respite using them 
+		// because respite is computed between Out and In stamps in the such order.
+		var expectedRespites = [];
+
+		var protocol = reporter.CreateReport(targetEmployerID, targetDay, maxRespiteDuration);
+
+		console.log(protocol.Notifications);
+
+		protocol.IsSucceed.should.equal(true);
+		protocol.Result.should.deep.equal(expectedRespites);
+
+	});
+
 });

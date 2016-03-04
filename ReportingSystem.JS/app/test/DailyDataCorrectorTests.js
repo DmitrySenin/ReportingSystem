1ca5913ab45aff68b1b1838a8045fafbab546d04
dmitry.senin <dmitry.senin@akvelon.com>
2016-03-04 14:05:49 +0300

    [JS]Gather stamps tests. Extarct test of eonstructor to separated file.

--- ReportingSystem.JS/app/test/DailyDataCorrectorTests.js
+++ ReportingSystem.JS/app/test/DailyDataCorrectorTests.js
@@ -8,20 +8,6 @@ var FakeStampsSource = require('./FakeStampsSource.js');
 
 describe("DailyDataCorrector", function() {
 
-	// Tests of constructor.
-	describe("#DailyDataCorrector", function() {
-
-		it("Should throw error if source of data is undefined.", function() {
-			var dataSource = undefined;
-			(() => new DailyDataCorrector(dataSource)).should.throw(Error);
-		});
-
-		it("Should throw error if source of data is null.", function() {
-			var dataSource = null;
-			(() => new DailyDataCorrector(dataSource)).should.throw(Error);
-		});
-	});
-
 	describe("#VerifyFirstStamp", function() {
 
 		it("Should add begging of target day as first In-stamp if there is no one.", function() {
@@ -116,6 +102,44 @@ describe("DailyDataCorrector", function() {
 
 			var expectedSize = targetDayStamps.length + 1;
 			var expectedStamp = allStamps[1];
+
+			dailyDataCorrector._verifyLastStamp(targetDayStamps, targetEmployerID, targetDay, notifications);
+
+			targetDayStamps.length.should.equal(expectedSize);
+			targetDayStamps[targetDayStamps.length - 1].should.deep.equal(expectedStamp);
+		});
+
+		it("Should add end of target day since first Out-stamp next does not satisfy restriction.", function() {
+			var targetEmployerID = 1;
+
+			// 03.04.2016
+			var targetDay = new Date(2016, 3, 3);
+
+			var nextDay = new Date(targetDay);
+			nextDay.setDate(nextDay.getDate() + 1);
+
+			// All stamps in source.
+			allStamps = [
+				// In-stamp at 9.00 am.
+				new EmployerTimeStamp( targetEmployerID, StampType.In, new Date(targetDay.setHours(9)) ),
+
+				// Out-stamp at 5.00 am of next day.
+				new EmployerTimeStamp( targetEmployerID, StampType.Out, new Date(nextDay.setHours(5, 00)) ),
+			];
+
+			// Stamps of target employer for target day.
+			targetDayStamps = [
+				allStamps[0]
+			];
+
+			// Create fake source.
+			var source = new FakeStampsSource.FakeStampsSource(allStamps);
+
+			var dailyDataCorrector = new DailyDataCorrector(source);
+			var notifications = [];
+
+			var expectedSize = targetDayStamps.length + 1;
+			var expectedStamp = new EmployerTimeStamp(targetEmployerID, StampType.Out, new Date(targetDay.setHours(23, 59, 59, 999)));;
 
 			dailyDataCorrector._verifyLastStamp(targetDayStamps, targetEmployerID, targetDay, notifications);
 

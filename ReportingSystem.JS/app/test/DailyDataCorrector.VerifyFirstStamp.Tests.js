6ad49096b7c0fe03fce1441c913e0fadfe86cc35
dmitry.senin <dmitry.senin@akvelon.com>
2016-03-04 14:36:09 +0300

    [JS]Gather stamps tests. Test that system does not change correct collection of stamps.

--- ReportingSystem.JS/app/test/DailyDataCorrector.VerifyFirstStamp.Tests.js
+++ ReportingSystem.JS/app/test/DailyDataCorrector.VerifyFirstStamp.Tests.js
@@ -56,5 +56,40 @@ describe("DailyDataCorrector.VerifyFirstStamp", function() {
 
 		(() => dailyDataCorrector._verifyFirstStamp(stamps, targetEmployerID, targetDay, notifications)).should.not.throw(Error);
 	});
+
+	it("Should not make any change since data is correct.", function() {
+
+		var targetEmployerID = 1;
+
+		// 03.04.2016
+		var targetDay = new Date(2016, 3, 3);
+
+		stamps = [
+			// In-stamp at 9.00 am
+			new EmployerTimeStamp( targetEmployerID, StampType.In, new Date(targetDay.setHours(9)) ),
+
+			// Out-stamp at 12.00 am
+			new EmployerTimeStamp( targetEmployerID, StampType.Out, new Date(targetDay.setHours(12)) ),
+
+			// In-stamp at 15.00 am
+			new EmployerTimeStamp( targetEmployerID, StampType.In, new Date(targetDay.setHours(15)) ),
+
+			// Out-stamp at 6.00 pm
+			new EmployerTimeStamp( targetEmployerID, StampType.Out, new Date(targetDay.setHours(18)) )
+		];
+
+		// Create fake source.
+		var source = new FakeStampsSource.FakeStampsSource(stamps);
+
+		var dailyDataCorrector = new DailyDataCorrector(source);
+		var notifications = [];
+
+		// Copy
+		var expectedStamps = stamps.slice();
+
+		dailyDataCorrector._verifyFirstStamp(stamps, targetEmployerID, targetDay, notifications);
+
+		stamps.should.deep.equal(expectedStamps);
+	});
 	
 });

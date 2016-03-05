041ed301fe8d2a80cecd17feebf61f6764f6b2cb
dmitry.senin <white.lord.mane@gmail.com>
2016-03-05 10:27:31 +0400

    [JS]Respites tests. Test that system finds all respites correctly if data contains mostakes.

--- ReportingSystem.JS/app/test/RespitesForDayReport.CreateReport.Tests.js
+++ ReportingSystem.JS/app/test/RespitesForDayReport.CreateReport.Tests.js
@@ -112,11 +112,64 @@ describe("RespitesForDayReport.CreateReport", function() {
 
 		var protocol = reporter.CreateReport(targetEmployerID, targetDay, maxRespiteDuration);
 
-		console.log(protocol.Notifications);
-
 		protocol.IsSucceed.should.equal(true);
 		protocol.Result.should.deep.equal(expectedRespites);
 
+	});
+
+	it("Should find respites correctly since data for day contains mistakes.", function() {
+
+		// Arrange
+	
+		var targetEmployerID = 1;
+
+		// 03.04.2016
+		var targetDay = new Date(2016, 3, 3);
+
+		var maxRespiteDuration = TimeSpan.fromMinutes(15); 
+
+		stamps = [
+			// Out-stamp at 9.30 am
+			new EmployerTimeStamp( targetEmployerID, StampType.Out, new Date(targetDay.setHours(9, 30)) ),
+
+			// Out-stamp at 10.00 am
+			new EmployerTimeStamp( targetEmployerID, StampType.Out, new Date(targetDay.setHours(10, 0)) ),
+
+			// In-stamp at 10.14 am
+			new EmployerTimeStamp( targetEmployerID, StampType.In, new Date(targetDay.setHours(10, 14)) ),
+
+			// In-stamp at 6.00 pm
+			new EmployerTimeStamp( targetEmployerID, StampType.In, new Date(targetDay.setHours(18)) ),
+
+			// In-stamp at 6.00 pm
+			new EmployerTimeStamp( targetEmployerID, StampType.In, new Date(targetDay.setHours(18)) ),
+		];
+
+		// Create fake source.
+		var source = new FakeStampsSource.FakeStampsSource(stamps);
+		var reporter = new RespitesForDayReport.RespitesForDay(source);
+
+		// Result should be like this because 
+		// system makes the following changes in data:
+		// 		1. Creates In-stamp at 12.00 am of target day
+		// 		2. Insert In-stamp at 9.45 am between Out-stamp at 9.30 am 
+		//												and Out-stamp at 10.00 am
+		//		3. Delete one of In-stamp at 6.00 pm
+		// 		4. Creates Out-stamps at 11.59.59:999 pm of target day
+		// 	So the following respites should be found:
+		// 		1. Between Out-stamp at 9.30 am and In-stamp at 9.45 am
+		// 		2. Between Out-stamp at 10.00 am and In-stamp at 10.14 am.
+		var expectedRespites = [
+			new Respite.Respite(stamps[0].Time, new Date(targetDay.setHours(9, 45))),
+			new Respite.Respite(stamps[1].Time, stamps[2].Time)
+		];
+
+		// Act
+		var protocol = reporter.CreateReport(targetEmployerID, targetDay, maxRespiteDuration);
+
+		// Assert
+		protocol.IsSucceed.should.equal(true);
+		protocol.Result.should.deep.equal(expectedRespites);
 	});
 
 });
